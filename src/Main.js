446fd1b54acf450023c460fae801ef384ac7ac3d
HelloWorld-n <gajica.b@gmail.com>
2022-09-12 14:58:35 +0200

    modified:   Main.js

--- src/Main.js
+++ src/Main.js
@@ -7,6 +7,15 @@ const TM_GOAL = 3
 const baseData = require('./data.js')
 let data = {}
 
+let debug = 0
+process.argv.forEach((val, index) => {
+	if (index > 1) {
+		if (val === "--debug") {
+			debug = true
+		}
+	}
+})
+
 try {
 	data = require('./.data.js')
 } catch (err) { }
@@ -41,4 +50,29 @@ function improve() {
 	fs.writeFile('./.data.js', `#!/bin/node\n\nmodule.exports = ${displayData(data)}\n`, () => { })
 }
 
-setInterval(improve, 1000)
+setInterval(() => {
+	improve()
+	console.log("\u001Bc")
+	console.log(displayData(data))
+}, 1000)
+
+let readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.on('keypress', (ch, key) => {
+	if (debug) {
+		console.log('got "keypress"', ch, key);
+	}
+	if (key && key.ctrl && !key.shift && key.name === 'c') {
+		process.exit()
+	}
+	if (key && !key.ctrl && !key.shift && key.name === 'i') {
+		if (Math.random() > 0.999) {
+			data.CLICK += 1
+			if (debug) {
+				console.log("INCREASED")
+			}
+		}
+	}
+});
+process.stdin.setRawMode(true);
+process.stdin.resume();
